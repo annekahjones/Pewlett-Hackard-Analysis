067025027f8aaf352622a0fcce1c67e4c8da250d
annekahjones <annekahjones@annekahs-air.lan>
2023-01-09 19:48:41 -0800

    Created queries for new tables and other actions

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -36,13 +36,13 @@ CREATE TABLE salaries (
 );
 
 CREATE TABLE dept_emp (
-	dept_no VARCHAR(4) NOT NULL,
 	emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (dept_no, emp_no)
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY (emp_no, dept_no)
 );
 
 CREATE TABLE titles (
@@ -51,7 +51,12 @@ CREATE TABLE titles (
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	PRIMARY KEY (emp_no, title, from_date)
 );
 
 SELECT * FROM departments;
+SELECT * FROM employees;
+SELECT * FROM dept_manager;
+SELECT * FROM salaries;
+SELECT * FROM dept_emp;
+SELECT * FROM titles;
